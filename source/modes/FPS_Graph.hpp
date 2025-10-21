fe9bbf247c68f977c07c16285a6a05fd2e4b5656
Damien Zhao <zdm65477730@126.com>
2025-10-22 00:00:33 +0800

    add multi-lang support
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- source/modes/FPS_Graph.hpp
+++ source/modes/FPS_Graph.hpp
@@ -6,8 +6,10 @@ private:
 	FpsGraphSettings settings;
 public:
 	bool isStarted = false;
-    com_FPSGraph() { 
-		GetConfigSettings(&settings);
+	com_FPSGraph() {
+		tsl::hlp::doWithSDCardHandle([this] {
+			GetConfigSettings(&settings);
+		});
 		switch(settings.setPos) {
 			case 1:
 			case 4:
@@ -27,7 +29,7 @@ public:
 			svcSleepThread(100'000);
 			SaltySD_Term();
 		}
-		alphabackground = 0x0;
+		IsFrameBackground = false;
 		tsl::hlp::requestForeground(false);
 		FullMode = false;
 		TeslaFPS = settings.refreshRate;
@@ -40,7 +42,7 @@ public:
 			tsl::gfx::Renderer::getRenderer().setLayerPos(0, 0);
 		FullMode = true;
 		tsl::hlp::requestForeground(true);
-		alphabackground = 0xD;
+		IsFrameBackground = true;
 		deactivateOriginalFooter = false;
 	}
 
@@ -68,11 +70,10 @@ public:
 	s16 y_60FPS = rectangle_y;
 	bool isAbove = false;
 
-    virtual tsl::elm::Element* createUI() override {
+	virtual tsl::elm::Element* createUI() override {
 		rootFrame = new tsl::elm::OverlayFrame("", "");
 
 		auto Status = new tsl::elm::CustomDrawer([this](tsl::gfx::Renderer *renderer, u16 x, u16 y, u16 w, u16 h) {
-
 			if (refreshRate && refreshRate < 240) {
 				rectangle_height = refreshRate;
 				rectangle_range_max = refreshRate;
@@ -91,7 +92,7 @@ public:
 				y_30FPS = rectangle_y+(rectangle_height / 2);
 				range = std::abs(rectangle_range_max - rectangle_range_min) + 1;
 			};
-			
+
 			switch(settings.setPos) {
 				case 1:
 					base_x = 224 - ((rectangle_width + 21) / 2);
@@ -130,7 +131,7 @@ public:
 			renderer->drawString(&legend_min[0], false, base_x+(rectangle_x-10), base_y+(rectangle_y+rectangle_height+3), 10, renderer->a(settings.minFPSTextColor));
 
 			size_t last_element = readings.size() - 1;
-			
+
 			s16 offset = 0;
 			if (refreshRate >= 100) offset = 7;
 
@@ -141,9 +142,9 @@ public:
 				}
 				else if (y_on_range > range) {
 					isAbove = true;
-					y_on_range = range; 
+					y_on_range = range;
 				}
-				
+
 				s16 y = rectangle_y + static_cast<s16>(std::lround((float)rectangle_height * ((float)(range - y_on_range) / (float)range))); // 320 + (80 * ((61 - 61)/61)) = 320
 				auto colour = renderer->a(settings.mainLineColor);
 				if (y == y_old && !isAbove && readings[last_element].zero_rounded) {
@@ -158,11 +159,11 @@ public:
 				}
 				/*
 				else if (y - y_old > 0) {
-					if (y_old + 1 <= rectangle_y+rectangle_height) 
+					if (y_old + 1 <= rectangle_y+rectangle_height)
 						y_old += 1;
 				}
 				else if (y - y_old < 0) {
-					if (y_old - 1 >= rectangle_y) 
+					if (y_old - 1 >= rectangle_y)
 						y_old -= 1;
 				}
 				*/
@@ -211,10 +212,10 @@ public:
 				lastFrame = 0;
 			}
 			FPSavg_c[0] = 0;
-		}
-		
+		}	
 	}
-	virtual bool handleInput(uint64_t keysDown, uint64_t keysHeld, touchPosition touchInput, JoystickPosition leftJoyStick, JoystickPosition rightJoyStick) override {
+
+	virtual bool handleInput(uint64_t keysDown, uint64_t keysHeld, const HidTouchState &touchPos, HidAnalogStickState leftJoyStick, HidAnalogStickState rightJoyStick) override {
 		if (isKeyComboPressed(keysHeld, keysDown, mappedButtons)) {
 			TeslaFPS = 60;
 			tsl::goBack();
--- source/modes/Resolutions.hpp
+++ source/modes/Resolutions.hpp
@@ -6,7 +6,9 @@ private:
 	ResolutionSettings settings;
 public:
     ResolutionsOverlay() {
-		GetConfigSettings(&settings);
+		tsl::hlp::doWithSDCardHandle([this] {
+			GetConfigSettings(&settings);
+		});
 		switch(settings.setPos) {
 			case 1:
 			case 4:
@@ -22,7 +24,7 @@ public:
 		StartFPSCounterThread();
 		deactivateOriginalFooter = true;
 		tsl::hlp::requestForeground(false);
-		alphabackground = 0x0;
+		IsFrameBackground = false;
 		FullMode = false;
 		TeslaFPS = settings.refreshRate;
 	}
@@ -31,7 +33,7 @@ public:
 		EndFPSCounterThread();
 		deactivateOriginalFooter = false;
 		tsl::hlp::requestForeground(true);
-		alphabackground = 0xD;
+		IsFrameBackground = true;
 		FullMode = true;
 		if (settings.setPos)
 			tsl::gfx::Renderer::getRenderer().setLayerPos(0, 0);
@@ -44,7 +46,7 @@ public:
 
     virtual tsl::elm::Element* createUI() override {
 		rootFrame = new tsl::elm::OverlayFrame("", "");
-		
+
 		auto Status = new tsl::elm::CustomDrawer([this](tsl::gfx::Renderer *renderer, u16 x, u16 y, u16 w, u16 h) {
 			int base_y = 0;
 			int base_x = 0;
@@ -76,15 +78,15 @@ public:
 				case 8:
 					base_x = 96;
 					base_y = 520;
-					break;	
+					break;
 			}
 
 			if (gameStart && NxFps -> API >= 1) {
 				renderer->drawRect(base_x, base_y, 360, 200, a(settings.backgroundColor));
 
-				renderer->drawString("Depth:", false, base_x+20, base_y+20, 20, renderer->a(settings.catColor));
+				renderer->drawString("DepthResolutionsOverlayCustomDrawerText"_tr.c_str(), false, base_x+20, base_y+20, 20, renderer->a(settings.catColor));
 				renderer->drawString(Resolutions_c, false, base_x+20, base_y+55, 18, renderer->a(settings.textColor));
-				renderer->drawString("Viewport:", false, base_x+180, base_y+20, 20, renderer->a(settings.catColor));
+				renderer->drawString("ViewportResolutionsOverlayCustomDrawerText"_tr.c_str(), false, base_x+180, base_y+20, 20, renderer->a(settings.catColor));
 				renderer->drawString(Resolutions2_c, false, base_x+180, base_y+55, 18, renderer->a(settings.textColor));
 			}
 			else {
@@ -97,7 +99,7 @@ public:
 						break;
 				}
 				renderer->drawRect(base_x, base_y, 360, 28, a(settings.backgroundColor));
-				renderer->drawString("Game is not running or it's incompatible.", false, base_x, base_y+20, 18, renderer->a(0xF00F));
+				renderer->drawString("GamaNotRunningOrIncompatibleResolutionsOverlayCustomDrawerText"_tr.c_str(), false, base_x, base_y+20, 18, renderer->a(0xF00F));
 			}
 		});
 
@@ -166,7 +168,7 @@ public:
 			resolutionLookup = false;
 		}
 	}
-	virtual bool handleInput(uint64_t keysDown, uint64_t keysHeld, touchPosition touchInput, JoystickPosition leftJoyStick, JoystickPosition rightJoyStick) override {
+	virtual bool handleInput(uint64_t keysDown, uint64_t keysHeld, const HidTouchState &touchPos, HidAnalogStickState leftJoyStick, HidAnalogStickState rightJoyStick) override {
 		if (isKeyComboPressed(keysHeld, keysDown, mappedButtons)) {
 			tsl::goBack();
 			return true;
